8134124456626817974f96e4e1fde7ffff970344
Brandon Bires-Navel <brandonnavel@outlook.com>
2019-09-18 14:24:57 -0400

    Modified the action space function to parse the game board to determine where the ship is

--- gym-play1.py
+++ gym-play1.py
@@ -6,9 +6,15 @@ import time
 import gym
 from gym import wrappers, logger
 
+from models import Ship, GameBoard
+from util import print_board
+
+
+
 
 class Agent(object):
     last_action = 0
+    last_ship = None
 
     """The world's simplest agent!"""
     def __init__(self, action_space):
@@ -16,12 +22,50 @@ class Agent(object):
 
     # You should modify this function
     def act(self, observation, reward, done):
-        if self.last_action is 5:
-            self.last_action = 4
-            return 4
-        else:
-            self.last_action = 5
-            return 5
+        zero_count = non_zero_count = 0
+        asteroids = None
+        board = GameBoard(observation)
+
+        rgb_colors = set()
+        ship_pixels = []
+
+        x, y = 0, 0
+
+        print_board(board)
+
+        while x < len(observation):
+            while y < len(observation[0]):
+                rgb_vals = observation[x][y]
+                if board.check_pixel(x, y, GameBoard.BLANK_RGB):
+                    zero_count += 1
+                elif board.check_pixel(x, y, GameBoard.SHIP_RGB):
+                    ship_pixels.append([x, y])
+                else:
+                    # print(f'POS: {x}, {y}')
+                    rgb_string = f'{rgb_vals[0]}, {rgb_vals[1]}, {rgb_vals[2]}'
+                    # print(f'RGB: {rgb_string}')
+                    non_zero_count += 1
+                    rgb_colors.add(rgb_string)
+                y += 1
+            x += 1
+            y = 0
+
+        for rgb in rgb_colors:
+            print(f'RGB Color: {rgb}')
+        if len(ship_pixels) is not 0:
+            self.last_ship = Ship(ship_pixels, self.last_ship)
+
+        print(f'Zero: {zero_count}, Non Zero: {non_zero_count}')
+        return 3
+        # if self.last_action is 5:
+        #     self.last_action = 4
+        #     return 4
+        # else:
+        #     self.last_action = 5
+        #     return 5
+
+
+
 
 
 # YOU MAY NOT MODIFY ANYTHING BELOW THIS LINE OR USE
@@ -54,15 +98,16 @@ if __name__ == '__main__':
     special_data = {}
     special_data['ale.lives'] = 3
     ob = env.reset()
+    i = 0
     while not done:
-        
+        i += 1
         action = agent.act(ob, reward, done)
         ob, reward, done, x = env.step(action)
         # pdb.set_trace()
-        time.sleep(20/1000)
         score += reward
         env.render()
-     
+        time.sleep(20/1000)
+
     # Close the env and write monitor result info to disk
     print ("Your score: %d" % score)
     env.close()
