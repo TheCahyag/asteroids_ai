ecfae65b607ed0fbc0aa47d8937c560ecbb16204
Brandon Bires-Navel <brandonnavel@outlook.com>
2019-09-18 14:24:18 -0400

    Added function to print the game board

---
+++ util.py
@@ -0,0 +1,19 @@
+import math
+
+from models import GameBoard
+
+
+def print_board(board: GameBoard):
+    string = ''
+    x, y = 0, 0
+    while x < len(board.game_map):
+        while y < len(board.game_map[0]):
+            if board.check_pixel(x, y, GameBoard.BLANK_RGB):
+                string = string + '.'
+            else:
+                string = string + 'x'
+            y += 1
+        x += 1
+        y = 0
+        string = string + '\n'
+    print(string)
