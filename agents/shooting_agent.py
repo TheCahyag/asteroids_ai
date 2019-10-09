dd8dc2e60f5d03f3d697fe3487ba97511e018b21
Brandon Bires-Navel <brandonnavel@outlook.com>
2019-10-09 10:52:47 -0400

    Added first agent which is a shooting agent

---
+++ agents/shooting_agent.py
@@ -0,0 +1,18 @@
+from agents.abstract_agent import AbstractAgent
+from models.actions import AsteroidsActions
+
+
+class ShootingAgent(AbstractAgent):
+
+    def __init__(self, action_space):
+        super().__init__(action_space)
+
+    def act(self, observation, reward: int, done: bool) -> int:
+        # This processes the data from the observation
+        super().act(observation, reward, done)
+        if done:
+            return AsteroidsActions.DO_NOTHING.value
+        self.frame += 1
+        return AsteroidsActions.FIRE_TURN_RIGHT.value
+
+
